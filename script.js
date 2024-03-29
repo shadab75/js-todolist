36c413273dc3d42ae6a4a11c187290cb29d00c6d
shadab75 <80451213+shadab75@users.noreply.github.com>
2024-03-29 12:49:45 +0330

    second commit

--- script.js
+++ script.js
@@ -1,34 +1,106 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const inputInvalid = document.getElementById('input-invalid');
-const  itemList = document.getElementById('item-list');
-function addItem(e)
-{
-    e.preventDefault();
-    const  newItem = itemInput.value;
+const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('items-clear');
+const itemFilter = document.getElementById('filter');
 
-    //validate Input
-    if (newItem==''){
+
+function addItem(e) {
+    e.preventDefault();
+
+    const newItem = itemInput.value;
+
+    // Validate Input
+    if (newItem == '') {
         inputInvalid.innerText = 'Please add an item';
         return;
-    }else{
-        inputInvalid.innerText='';
+    } else {
+        inputInvalid.innerText = '';
     }
+
+ addItemToDOM(newItem)
+ addItemtoStorage(newItem);
+    itemInput.value = '';
+    checkUI();
+}
+
+function addItemToDOM(item)
+{
     const li = document.createElement('li');
     li.className = 'list-item';
-    li.textContent=newItem;
-    const icon = createIcon('bi bi-x fs-5 text-danger');
-    li.appendChild(icon)
-    itemList.appendChild(li)
-    itemInput.value ='';
+    li.textContent = item;
 
+    const icon = createIcon('bi bi-x fs-5 text-danger');
+    li.appendChild(icon);
+
+    itemList.appendChild(li);
 }
-function createIcon(classes){
-    const  icon = document.createElement('i');
+function addItemtoStorage(item)
+{
+    let itemsFromStorage;
+    if (localStorage.getItem('items')==null)
+    {
+        itemsFromStorage =[];
+    }else{
+        itemsFromStorage = JSON.parse(localStorage.getItem('items'))
+    }
+    itemsFromStorage.push(item);
+    localStorage.setItem('items',JSON.stringify(itemsFromStorage))
+}
+
+function createIcon(classes) {
+    const icon = document.createElement('i');
     icon.className = classes;
+
     return icon;
 }
 
+function onClickItem(e) {
+    // e.target.parentElement.remove();
+    // console.log(e.target.classList.contains('bi-x'));
+    if (e.target.classList.contains('bi-x')) {
+        e.target.parentElement.remove();
+        checkUI();
+    }
+}
 
-//Event Listener
-itemForm.addEventListener('submit',addItem);
+function clearItems() {
+    itemList.innerHTML = '';
+    checkUI();
+}
+
+function checkUI() {
+    const items = itemList.querySelectorAll('li');
+
+    if (items.length === 0) {
+        clearBtn.style.display = 'none';
+        itemFilter.style.display = 'none';
+    } else {
+        clearBtn.style.display = 'block';
+        itemFilter.style.display = 'block';
+    }
+}
+
+function filterItems(e) {
+    const items = itemList.querySelectorAll('li');
+    const text = e.target.value.toLowerCase();
+    items.forEach((item) => {
+        const itemName = item.firstChild.textContent.toLowerCase();
+        if (itemName.indexOf(text)!==-1)
+        {
+            item.style.display = 'flex';
+        }else{
+            item.style.display = 'none';
+        }
+
+    });
+}
+
+// Event Listener
+itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', onClickItem);
+clearBtn.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', filterItems);
+
+checkUI();
